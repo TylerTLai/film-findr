726d63c8d44fb679044deeb69f4e7a59c77592ae
Tyler Lai <lai.t.tyler@gmail.com>
2020-05-29 04:13:20 -0500

    Update menu on mobile.
    Update mobile menu.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,23 +1,63 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { NavLink } from 'react-router-dom';
 import SearchBar from './SearchBar';
+import { AiFillHome, AiFillStar } from 'react-icons/ai';
+import { FaBars, FaSearch } from 'react-icons/fa';
+import { BsFillBarChartFill } from 'react-icons/bs';
+// import { useSpring, animated as a, config } from 'react-spring';
 
 const Navbar = ({ movieData }) => {
+  const getMovieData = (movies) => {
+    movieData(movies);
+  };
 
-    const getMovieData = (movies) => {
-        movieData(movies);
-    };
+  return (
+    <>
+      <nav className="Navbar">
+        <NavLink to="top-rated">Top Rated</NavLink>
+        <NavLink to="/most-popular">Most Popular</NavLink>
+        <NavLink to="/">Home</NavLink>
+        <SearchBar movieData={getMovieData} />
+      </nav>
 
-    return (
-        <>
-            <nav className="Navbar">
-                <NavLink to="top-rated">Top Rated</NavLink>
-                <NavLink to="/most-popular">Most Popular</NavLink>
-                <NavLink to="/">Home</NavLink>
-                <SearchBar movieData={getMovieData} />
-            </nav>
-        </>
-    );
+      {/* mobile menu */}
+
+      <div className="MenuBars">
+        <FaBars style={{ fontSize: '1.8em', marginBottom: '.5em' }} />
+      </div>
+
+      <nav className="NavbarMobile">
+        <ul>
+          <li>
+            <NavLink to="top-rated" className="NavbarMobileIcons">
+              <AiFillStar />
+              <p>TOP RATED</p>
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/most-popular" className="NavbarMobileIcons">
+              <BsFillBarChartFill />
+              <p>MOST POPULAR</p>
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/search" className="NavbarMobileIcons">
+              <FaSearch />
+              <p>SEARCH</p>
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/" className="NavbarMobileIcons">
+              <AiFillHome />
+              <p>HOME</p>
+            </NavLink>
+          </li>
+        </ul>
+
+        {/* <SearchBar movieData={getMovieData} /> */}
+      </nav>
+    </>
+  );
 };
 
 export default Navbar;
