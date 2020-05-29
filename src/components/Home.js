986d8868f56c73fa9b237a1dd3fdb6d95f738d16
Tyler Lai <lai.t.tyler@gmail.com>
2020-05-29 15:36:59 -0500

    Update Mobile menu
    Hide menu when scrolling down. Change links to gray.
    Change active link to blue.
    Reduce font size.

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,32 +1,39 @@
 import React from 'react';
 import MostPopular from './MostPopular';
 import TopRated from './TopRated';
-import MovieCardList from './MovieCardList';
 import SlideView from './SlideView';
 
 const Home = ({ movieData }) => {
+  // const clearSearchResults = () => {
+  //   alert('clear search results');
+  // };
 
-  const clearSearchResults = () => {
-    alert('clear search results');
-  };
+  // return (
+  //   <div>
+  //     {/* {console.log('home render', movieData)} */}
+
+  //     {movieData && movieData.length ? (
+  //       <>
+  //         <MovieCardList movies={movieData} />
+  //       </>
+  //     ) : (
+  //       <>
+  //         <SlideView />
+  //         <MostPopular />
+  //         <br />
+  //         <TopRated />
+  //       </>
+  //     )}
+  //   </div>
+  // );
 
   return (
-    <div>
-      {/* {console.log('home render', movieData)} */}
-
-      {movieData && movieData.length ? (
-        <>
-          <MovieCardList movies={movieData} />
-        </>
-      ) : (
-        <>
-          <SlideView />
-          <MostPopular />
-          <br />
-          <TopRated />
-        </>
-      )}
-    </div>
+    <>
+      <SlideView />
+      <MostPopular />
+      <br />
+      <TopRated />
+    </>
   );
 };
 
